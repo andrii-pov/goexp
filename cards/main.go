9ba6d7087e97c571267c13edc09c569846b0f979
AndreyPov <povoroznuka.m@gmail.com>
2018-05-28 08:35:56 +0300

    added cards

--- cards/main.go
+++ cards/main.go
@@ -6,13 +6,11 @@ func main() {
 	//var card string = "Ace of Spades"
 	card := "Ace of Spades" //automatically finds out the type by :=
 	card = newCard()        //reassign - no need in := anymore
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, card)
 	fmt.Println(card)
 	fmt.Println(cards)
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
