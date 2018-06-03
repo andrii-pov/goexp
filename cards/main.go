61d2541ab77374dfd979c05588cac7b47f1b9e33
AndreyPov <povoroznuka.m@gmail.com>
2018-06-04 00:45:47 +0300

    added array manipulations

--- cards/main.go
+++ cards/main.go
@@ -6,11 +6,14 @@ func main() {
 	//var card string = "Ace of Spades"
 	card := "Ace of Spades" //automatically finds out the type by :=
 	card = newCard()        //reassign - no need in := anymore
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, card)
+	cards := newDeck()      // deck{"Ace of Diamonds", newCard()}
+	//cards = append(cards, card)
 	fmt.Println(card)
 	fmt.Println(cards)
 	cards.print()
+	hand, remainingValues := deal(cards, 5)
+	hand.print()
+	remainingValues.print()
 }
 
 func newCard() string {
