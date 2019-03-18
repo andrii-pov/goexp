aa16bd7f3a4f586798802f53cdb4f9a5e5fda2da
AndreyPov <povoroznuka.m@gmail.com>
2019-03-19 01:23:47 +0200

    added channel experiments with select, timer

---
+++ select.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		c1 <- "First"
+	}()
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "Second"
+	}()
+
+	for i := 0; i < 2; i++ { // putting here 3 will provoke deadlock. The channel length should be specified here
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+		}
+	}
+}
