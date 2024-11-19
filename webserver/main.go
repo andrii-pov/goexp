ade1062b0e1b05854844a0e734b545830e857415
Andrii Povorozniuk <povoroznuka.m@gmail.com>
2024-11-19 21:49:26 +0100

    chore(web): small-server

---
+++ webserver/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func sayHelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()       // parse arguments, you have to call this by yourself
+	fmt.Println(r.Form) // print form information in server side
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println("url_long: ", r.Form["url_long"])
+	fmt.Println("------")
+	for k, v := range r.Form {
+		fmt.Println("key: ", k)
+		fmt.Println("val: ", strings.Join(v, ""))
+	}
+	fmt.Println("------")
+	fmt.Fprintf(w, "Hello Andrii!") // send data to client side
+}
+
+func main() {
+	http.HandleFunc("/", sayHelloName)       // setting router
+	err := http.ListenAndServe(":9090", nil) // setting listening port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
