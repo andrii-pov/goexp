73867c5e2495b4a71815cba1a6e48f185d1619c2
Andrii Povorozniuk <povoroznuka.m@gmail.com>
2025-05-23 13:08:43 +0200

    time-channel-profiling-solution

--- time_channels_atomic.go
+++ time_channels_atomic.go
@@ -4,34 +4,41 @@ import (
 	"fmt"
 	"math/rand/v2"
 	"sync"
-	"sync/atomic"
 	"time"
 )
 
 var maxWaitSeconds = 5
-var totalWorkSeconds atomic.Int32
 
-func randomWait(wg *sync.WaitGroup) {
+func randomWait(wg *sync.WaitGroup, ch chan int) {
 	workSeconds := rand.IntN(maxWaitSeconds + 1)
 
 	time.Sleep(time.Duration(workSeconds) * time.Second)
-	fmt.Printf("Work seconds: %d\n", workSeconds)
 
-	totalWorkSeconds.Add(int32(workSeconds))
 	wg.Done()
+	ch <- workSeconds
+
 	return
 }
 
 func main() {
 	mainSeconds := 0
 	wg := new(sync.WaitGroup)
+	ch := make(chan int)
+	start := time.Now()
 	wg.Add(100)
 	for range 100 {
-		go randomWait(wg)
+		go randomWait(wg, ch)
 	}
 	wg.Wait()
 
-	fmt.Println("main: ", mainSeconds)
-	fmt.Println("total work seconds: ", totalWorkSeconds.Load())
+	var currSeconds int
+	for range 100 {
+		currSeconds = <-ch
+		fmt.Println(currSeconds)
+		mainSeconds += currSeconds
+	}
+	close(ch)
 
+	fmt.Println("Main goroutine: ", time.Since(start))
+	fmt.Println("main: ", mainSeconds)
 }
